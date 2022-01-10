ef15d25d3b700522d6eb185e6ea89cfc1add77cc
ishaangarg015 <ishaangarg.bt21cse@pec.edu.in>
2022-01-10 15:15:32 +0530

    Add files via upload

---
+++ Assignment1_Output.py
@@ -0,0 +1,30 @@
+Enter first number: 45
+Enter second number: 34
+Enter third number: 99
+The average of the three numbers is  59.333333333333336
+
+Enter the gross income to the nearest penny: 1234000.00
+Enter number of dependents: 4
+Payable tax is:  242400.0
+
+Enter SID: 21103015
+Enter Name: Ishaan Garg
+Enter Gender: M
+Enter Course Name: Introduction to Computing
+Enter CGPA: 9.6
+Student Details: [21103015, 'Ishaan Garg', 'M', 'Introduction to Computing', 9.6]
+
+Input marks 1
+89
+Input marks 2  
+95
+Input marks 3  
+86
+Input marks 4  
+76
+Input marks 5  
+99
+Marks of students in ascending order are:  [76, 86, 89, 95, 99]
+
+List after removing 'Black':  ['Red', 'Green', 'White', 'Pink', 'Yellow']
+List after replacing 'Black' and 'Pink' with 'Purple'  ['Red', 'Green', 'White', 'Purple', 'Yellow']
